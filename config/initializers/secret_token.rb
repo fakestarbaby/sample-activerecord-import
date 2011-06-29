4c5802cfe860157c9ad34ee4247cb24ac3918cde
fakestarbaby <fakestarbaby@gmail.com>
2011-06-29 12:19:48 +0900

    初回コミット。

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleActiverecordImport::Application.config.secret_token = '2a7319a5479015bf7d92df522a09735a1d2781f81d7e7ae4c2b25c5cb5b227f1abd94d396e1718b1d33f09cbf50a4a3920cba6312e887b3288c0922d88e52e98'
